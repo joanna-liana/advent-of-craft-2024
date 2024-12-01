00b417a5442a85552c5c3241087654a906ad8e74
Joanna Liana <joanna_liana@outlook.com>
2024-12-01 14:42:00 +0100

    Day 1 - expect message object as param

--- solution-mine/day01/tests/santaCommunicator.spec.ts
+++ solution-mine/day01/tests/santaCommunicator.spec.ts
@@ -6,6 +6,13 @@ const NORTH_POLE = 'North Pole';
 const numberOfDaysToRest = 2;
 const numberOfDayBeforeChristmas = 24;
 
+type MessageData = {
+    reindeerName: string;
+    currentLocation: string;
+    numbersOfDaysForComingBack: number;
+    numberOfDaysBeforeChristmas: number;
+};
+
 describe('SantaCommunicator', () => {
     let communicator: SantaCommunicator;
     let logger: TestLogger;
@@ -16,7 +23,18 @@ describe('SantaCommunicator', () => {
     });
 
     test('composeMessage', () => {
-        const message = communicator.composeMessage(SantaCommunicatorSpec, NORTH_POLE, 5, numberOfDayBeforeChristmas);
+        // given
+        const messageData: MessageData = {
+            reindeerName: SantaCommunicatorSpec,
+            currentLocation: NORTH_POLE,
+            numbersOfDaysForComingBack: 5,
+            numberOfDaysBeforeChristmas: numberOfDayBeforeChristmas
+        }
+
+        // when
+        const message = communicator.composeMessage(messageData);
+
+        // then
         expect(message).toEqual('Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas.');
     });
 
